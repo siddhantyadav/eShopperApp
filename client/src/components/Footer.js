dd7ac0db8702eced4e49a7616ab64b67c7be2002
Siddhant Yadav <sidd.yadav15@gmail.com>
2021-01-10 21:33:20 -0500

    added HomeScreen and ProductScreen

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-  return <footerFooter>Footer</footerFooter>;
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; eShopperApp</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
